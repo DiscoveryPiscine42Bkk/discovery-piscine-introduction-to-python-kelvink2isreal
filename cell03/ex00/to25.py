eecc0f6cfd4ff1299da085384c0e4c099bd6eaf2
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-23 17:27:21 +0000

    While Loop

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number less than 25\n"))
+if num > 25:
+    print("Error")
+if num < 25 or num == 25:
+    i = num
+    while i <= 25:
+        print(f"Inside the loop, myy variable is {i}")
+        i += 1
+
