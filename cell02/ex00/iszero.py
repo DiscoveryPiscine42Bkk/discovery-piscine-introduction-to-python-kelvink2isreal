a0bc8855cf4ff0bb6f08464e7f322e6f1dc9dfff
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-23 16:47:25 +0000

    Conditional Statement

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+if num == 0:
+    print("This number is equal to zero") #without indention there will be an error
+else:
+    print("This number is different from zero")
