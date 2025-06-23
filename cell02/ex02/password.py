8e01792aa595fc345083ba0538b72d3cd566bc55
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-23 17:04:19 +0000

    Asking Password

---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "Python is awesome"
+password_input = input("Enter your password: ")
+if password_input== "Python is awesome":
+    print("ACCESS GRANTED")
+else: 
+    print("ACCESS DENIED")
