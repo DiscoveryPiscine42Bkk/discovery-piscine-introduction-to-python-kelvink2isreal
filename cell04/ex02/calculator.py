111349d2437fe664206340c85f794c9f44bf479c
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-24 06:56:09 +0000

    Calculator

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+first_num = int(input("Give me the first number: "))
+sec_num = int(input("Give me the second number: "))
+print("Thank You!")
+print(f"{first_num} + {sec_num} = {first_num+sec_num}" )
+print(f"{first_num} - {sec_num} = {first_num-sec_num}" )
+print(f"{first_num} / {sec_num} = {first_num/sec_num}" )
+print(f"{first_num} x {sec_num} = {first_num*sec_num}" )
