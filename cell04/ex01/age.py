80664489ab32fabc95d540699c4793908a5cdce4
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-24 06:42:38 +0000

    Age Calculator

---
+++ cell04/ex01/age.py
@@ -0,0 +1,10 @@
+age = int(input("Please tell me your age: "))
+print(f"You are currently {age} years old")
+#age_10_years = age + 10
+#age_20_years = age + 10
+#age_30_years = age + 10
+i = 10 
+while i <=30:
+    j = age + i
+    print(f"In {i} years, you'll be {j} years old ")
+    i +=10
