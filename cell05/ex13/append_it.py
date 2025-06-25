107ba3585a28652d1303c5858bb5091fb2cc16ed
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-25 19:26:28 +0000

    .endwith & every word in arguments

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,13 @@
+import sys
+
+
+if len(sys.argv)<= 1:
+    print("none")
+else: 
+    x = sys.argv[1:]
+    for a in x:
+        if a.endswith("ism"):
+            continue
+        else:
+            print(a+"ism")
+
