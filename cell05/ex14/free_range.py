4cc86e061d842f4f53872f2ade22872648b21767
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-25 19:38:24 +0000

    Using range

---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv) <=2:
+    print("none")
+else: 
+    first = int(sys.argv[1])
+    second = int(sys.argv[2])
+    result = [a for a in range(first,second+1,1)] #range(start,length,interval)
+    print(result)
+        
