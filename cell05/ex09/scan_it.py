f0bb0d94a7b75b5bfbed0188862239b4ebff991c
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-25 17:56:16 +0000

    re.findall

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,10 +1,13 @@
 import sys
 import re
 
-if len(sys.argv) !=3:
+if len(sys.argv)!=3:
     print("none")
-else:
+else: 
     keyword = sys.argv[1]
     text = sys.argv[2]
-    param = re.findall(keyword,text) #re_module refindall()
-    print(len(param))
+    search = re.findall(keyword,text)
+    if len(search) == 0:
+        print("none")
+    else: 
+        print(len(search))
