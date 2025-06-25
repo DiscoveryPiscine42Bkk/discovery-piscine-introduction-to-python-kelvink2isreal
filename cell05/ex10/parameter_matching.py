22db894ec1bddf7d5effc5587e900128f73bbf39
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-25 18:16:07 +0000

    Matching Parameter

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,14 @@
+import sys
+import re
+
+if len(sys.argv)<2:
+    print("none")
+else: 
+    keyword = sys.argv[1]
+    param = input("What was the parameter? ")
+    find = (re.findall(keyword,param))
+    if len(find) == 1:
+        print("Good Job")
+    else: 
+        print("Nope, Sorry...")
+    
