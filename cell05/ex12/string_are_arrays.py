cb2cc3e2240cebfeaf159c0d640a2b62cc3ecaa4
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-25 19:05:46 +0000

    String is an array

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,18 @@
+import sys
+import re
+
+if len(sys.argv) == 1:
+    print("none")
+else: 
+    keyword = "z"
+    param = sys.argv[1]
+    find = re.findall(keyword,param)
+    if len(find) == 0:
+        print("none")
+    else: 
+        for x in param: #string is also an array 
+           if x == 'z':
+                print(f"{keyword}", end="")
+
+
+
