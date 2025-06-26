938602591bd7d8dbd11fb68c54a9f610b7a81b94
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-26 06:33:10 +0000

    Arrary Definition

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,15 @@
+def array_of_names(people):
+    names = []
+    for first,last in people.items():
+        name = f"{first.capitalize()} {last.capitalize()}"
+        names.append(name)
+    return names
+    
+persons = {
+"jean": "valjean",
+"grace": "hopper",
+"xavier": "niel",
+"fifi": "brindacier"
+}
+
+print(array_of_names(persons))
