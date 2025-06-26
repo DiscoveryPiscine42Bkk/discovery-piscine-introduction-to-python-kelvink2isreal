a2525c495114aa8600811eed568b7e0ac8501faf
kelvink2isreal <153079483+kelvink2isreal@users.noreply.github.com>
2025-06-26 05:58:35 +0000

    Methods

---
+++ cell06/ex04/methods_everywhere.py
@@ -0,0 +1,19 @@
+import sys
+words = sys.argv[1:]
+def shrink(x):
+    print(x[:8])
+def enlarge(x):
+    for i in range (len(x),8):
+      x += 'Z'
+    print(x)
+if len(sys.argv) <= 1:
+    print("none")
+else:
+    for j in words:
+        if len(j) < 8:
+            enlarge(j)
+        elif len(j) > 8:
+            shrink(j)
+        else:
+            print(j)
+
